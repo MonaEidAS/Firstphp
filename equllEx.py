92c2aa9d758ecb1a165d31c04fb870619c91b834
MonaEidAS <47875382+MonaEidAS@users.noreply.github.com>
2019-02-23 12:20:12 +0300

    Add files via upload

---
+++ equllEx.py
@@ -0,0 +1,16 @@
+a = input("Enter first number: ")
+
+b =input("Enter Second number: ")
+
+c = input("Enter Third number: ")
+
+d = input("Enter Fourth number: ")
+
+
+
+if (a == b) and (b == c) and (c == d):
+  print("Numbers are equall")
+
+else:
+
+   print("Numbers are not equall!!")
